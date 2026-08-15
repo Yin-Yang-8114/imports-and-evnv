f4c9ed18d04fae18e3aa5562565d7f8792f072f5
Yin Yang <mihiparis@gmail.com>
2026-08-15 23:22:31 +0300

    Section 2

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from datetime import date
+import questionary
 from rich.console import Console
 from rich.table import Table
 
 console = Console()
-
 expenses = [{"date": "2026-08-10", "title": "Notebook", "category": "school", "amount": 24.90},{"date": "2026-08-11", "title": "Coffee", "category": "food", "amount": 12.00},]
 
 
@@ -20,39 +20,42 @@ def show_expenses(expenses):
     table.add_column("Title", style="magenta")
     table.add_column("Category", style="green")
     table.add_column("Amount", justify="right", style="bold yellow")
-    for item in expenses:
-        table.add_row(item["date"],item["title"],item["category"],f"{item['amount']}")
-
+    for item in expenses:table.add_row(item["date"],item["title"],item["category"],f"{item['amount']:.2f}")
     console.print(table)
-
     total = calculate_total(expenses)
-    console.print(f"Total: {total} ILS\n")
-
+    console.print(f"[bold green]Total: {total:.2f} ILS[/bold green]\n")
 
 def add_expense(expenses, title, amount, category):
     new_item = {"date": date.today().isoformat(),"title": title,"amount": amount,"category": category}
     expenses.append(new_item)
 
 
-def ask_for_expense(expenses):
-    title = input("Expense title: ")
-    amount = float(input("Amount: "))
-    category = input("Category: ")
-    add_expense(expenses, title, amount, category)
+def is_valid_number(text):
+    try:
+        val = float(text)
+        if val <= 0:
+            return "Please enter a number greater than 0"
+        return True
+    except ValueError:
+        return "Please enter a valid number"
 
+
+def ask_for_expense(expenses):
+    title = questionary.text("Expense title:").ask()
+    amount_str = questionary.text("Amount:", validate=is_valid_number).ask()
+    amount = float(amount_str)
+    category = questionary.select("Choose category:",choices=["food", "travel", "school", "entertainment", "other"]).ask()
+    add_expense(expenses, title, amount, category)
 
 def main():
     show_expenses(expenses)
     while True:
-        choice = input("Would you like to add an expense? (y/n): ")
-        if choice.lower() == 'y':
+        add_more = questionary.confirm("Would you like to add an expense?").ask()
+        if add_more:
             ask_for_expense(expenses)
             show_expenses(expenses)
-        elif choice.lower() == 'n':
-            show_expenses(expenses)
-            break
         else:
-            print("Please enter either 'y' or 'n'")
+            break
 
 
 main()
